b779796d219b7a8eb55e6daf210be744da046380
lichengzhang2 <lichengzhangmath@gmail.com>
2025-04-05 17:16:02 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+// This program will produce the same output as program hello. Only difference is that this
+// program contains the comments. Comments are for the convenience of
+//programmers only.
+// Compiler simply ignores these comments.
+// BL
+#include <stdio.h> // LOC 1 
+
+// BL
+int main() { // LOC 2
+	// LOC 3
+	printf("Hello, world\n"); // LOC 4
+	return (0); // LOC 5
+} // LOC 6
