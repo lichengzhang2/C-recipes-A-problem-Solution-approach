59ba836a4a14c7e4fa2c7c7c6e3fed41dd69f708
lichengzhang2 <lichengzhangmath@gmail.com>
2025-04-05 17:17:27 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,13 +1,9 @@
-// This program will produce the same output as program hello. Only difference is that this
-// program contains the comments. Comments are for the convenience of
-//programmers only.
-// Compiler simply ignores these comments.
-// BL
-#include <stdio.h> // LOC 1 
+// This program will produce the same output as program hello.
+#include <stdio.h>
 
-// BL
-int main() { // LOC 2
-	// LOC 3
-	printf("Hello, world\n"); // LOC 4
-	return (0); // LOC 5
-} // LOC 6
+
+int main() {
+
+	printf("Hello, world\n");
+	return (0);
+}
